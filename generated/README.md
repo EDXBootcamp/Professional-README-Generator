a79e8782bc88d7b21a8522fb9019206a8b28c933
Tj Thouhid <tjthouhid@gmail.com>
2024-03-04 10:14:25 +0000

    added answers to the markdown

--- generated/README.md
+++ generated/README.md
@@ -1,2 +1,31 @@
+
 # test
 
+## Description
+test
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+test
+
+## Usage
+test
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+test
+
+## Tests
+test
+
+## Questions
+For additional questions, contact [test](https://github.com/test) via email at test.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For additional questions, contact [${data.github}](https://github.com/${data.github}) via email at ${data.email}.
 `;
 }
 
