7438399c83853e211cb4773e001453bc3045cf3d
Tj Thouhid <tjthouhid@gmail.com>
2024-03-04 10:13:03 +0000

    questions for readme added

--- index.js
+++ index.js
@@ -3,19 +3,77 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// Array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
 ];
 
-// function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md successfully generated!');
+        }
+    });
 }
 
-// function to initialize program
+// Function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((answers) => {
+            // Generate markdown content
+            const markdown = generateMarkdown(answers);
+            // Write markdown content to README.md file
+            writeToFile('generated/README.md', markdown);
+        });
 }
 
-// function call to initialize program
+// Call init function to start the application
 init();
